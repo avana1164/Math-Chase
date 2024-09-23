5b38bc6fdfb8bffb6014fbe9318b16ba0c95efda
AAnand05 <44912634+avana1164@users.noreply.github.com>
2024-09-22 22:46:26 -0400

    Revert "Third Commit"
    This reverts commit ce2ef5edf6717622371fdce7160f0325336ad887.

--- Overworld.js
+++ Overworld.js
@@ -4,34 +4,34 @@ class Overworld {
         this.element = config.element;
         this.canvas = this.element.querySelector(".game-canvas");
         this.ctx = this.canvas.getContext("2d");
-        this.map = null;
-    }
-
-    startGameLoop() {
-        const step = () => {
-            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-            
-            //the lower layer will draw the first layer of the map
-            this.map.drawLowerImage(this.ctx);
-
-            //this method draw all the characters in gameObjects
-            Object.values(this.map.gameObjects).forEach(object => {
-                object.x+=0.5;
-                object.sprite.draw(this.ctx);
-            })
-
-            //this upper layer of the map will prevent any character from being draw on top of the house
-            //this.map.drawUpperImage(this.ctx); 
-            requestAnimationFrame(() => {
-                step();
-            });
-        }
-        step();
     }
 
     init() {
         this.ctx.imageSmoothingEnabled = false;
-        this.map = new OverworldMap(window.OverworldMaps.GrassyField);
-        this.startGameLoop();
+        for(let i = 0; i < 16; i++){
+            for(let j = 0; j < 32; j++){
+                const image = new Image();
+                image.onload = () => {
+                    this.ctx.drawImage(image, 0, 0, 16, 16, j*16, i*16, 16, 16);
+                }
+                image.src = "/game_sprites/Overworld.png";
+            }    
+        }
+
+        const hero = new GameObject({
+            x: 15,
+            y: 20
+        });
+        
+        const npc = new GameObject({
+            x: 300,
+            y: 150,
+            src: "/game_sprites/NPC_test.png"
+        });
+
+        setTimeout(() => {
+            hero.sprite.draw(this.ctx);
+            npc.sprite.draw(this.ctx);
+        }, 200)
     }
 }
