ce2ef5edf6717622371fdce7160f0325336ad887
AAnand05 <44912634+avana1164@users.noreply.github.com>
2024-09-23 02:44:04 +0000

    Third Commit

---
+++ OverworldMap.js
@@ -0,0 +1,35 @@
+class OverworldMap {
+    constructor(config){
+        this.gameObjects = config.gameObjects; 
+        this.upperImage = new Image();
+        this.lowerImage = new Image();
+        this.lowerImage.src = config.lowerSrc;
+        this.upperImage.src = config.upperSrc;
+    }
+
+    drawLowerImage(ctx) {
+        ctx.drawImage(this.lowerImage, 0, 0);
+    }
+
+    drawUpperImage(ctx) {
+        ctx.drawImage(this.upperImage, 0, 0);
+    }
+}
+
+window.OverworldMaps = {
+    GrassyField: {
+        lowerSrc: "/game_sprites/grassy_field.png",
+        upperSrc: "/game_sprites/grassy_field.png",
+        gameObjects: {
+            hero: new GameObject({
+                x: 15,
+                y: 20
+            }),
+            npc: new GameObject({
+                x: 300,
+                y: 150,
+                src: "/game_sprites/NPC_test.png"
+            })
+        }
+    }
+}
