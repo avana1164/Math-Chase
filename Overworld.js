8eddab0bc9f31c4f0077816a9e48d32040bfdb2d
AAnand05 <44912634+avana1164@users.noreply.github.com>
2024-10-02 15:35:31 -0400

    Commit Restart #3
    The player keyboard movement has been fixed.

--- Overworld.js
+++ Overworld.js
@@ -18,38 +18,56 @@ field.src = "/game_sprites/grassy_field1.png";
 
 let x = 20;
 let y = 30;
-let vx = 0;
-let vy = 0;
-let speed = 5;
-let keyPressed = 'KeyS';
+let speed = 2;
+let keysPressed = {
+    KeyS: false,
+    KeyA: false,
+    KeyD: false,
+    KeyW: false 
+};
+let lastKey = 'KeyS';
 let keys = ['KeyS', 'KeyA', 'KeyW', 'KeyD'];
 let isMoving = false;
 window.addEventListener("keydown", (e) => {
-    if(e.code == 'KeyW' || e.code == 'KeyS' || e.code == 'KeyA' || e.code == 'KeyD'){
-        if (e.code == 'KeyW'){
-            vy = -speed;
-        }  
-        else if (e.code == 'KeyS'){
-            vy = speed;
-        } 
-        else if (e.code == 'KeyD'){
-            vx = speed;
+    if (keys.indexOf(e.code) != -1){
+        switch(e.code) {
+            case 'KeyS':
+                keysPressed.KeyS = true;
+                lastKey = 'KeyS';
+                break;
+            case 'KeyA':
+                keysPressed.KeyA = true;
+                lastKey = 'KeyA';
+                break;
+            case 'KeyW':
+                keysPressed.KeyW = true;
+                lastKey = 'KeyW';
+                break;
+            case 'KeyD':
+                keysPressed.KeyD = true;
+                lastKey = 'KeyD'
+                break;
         }
-        else if (e.code == 'KeyA'){
-            vx = -speed;
-        }
-        keyPressed = e.code;
     }
-        
 });
 
 
 
 window.addEventListener("keyup", (e) => {
-    if (e.code == 'KeyW') vy = 0; 
-    else if (e.code == 'KeyS') vy = 0;
-    else if (e.code == 'KeyD') vx = 0;
-    else if (e.code == 'KeyA') vx = 0;
+    switch(e.code) {
+        case 'KeyS':
+            keysPressed.KeyS = false;
+            break;
+        case 'KeyA':
+            keysPressed.KeyA = false;           
+            break;
+        case 'KeyW':
+            keysPressed.KeyW = false;        
+            break;
+        case 'KeyD':
+            keysPressed.KeyD = false;     
+            break;
+    }
 });
 
 let spriteX = 0;
@@ -58,9 +76,8 @@ let tickCount = 0;
 const ticksPerFrame = 10;
 update = () => {
     ctx.clearRect(0, 0, c.width, c.height);
-    x += vx/2;
-    y += vy/2;
-    let spriteY = spriteDirection[keyPressed];
+    moveCharacter();
+    let spriteY = spriteDirection[lastKey];
     ctx.drawImage(field, 0, 0, 512, 256, 0, 0, 1024, 512);
     zIndex = sort();
     for(let i = 0; i < obstacles.length; i++) {
@@ -74,11 +91,11 @@ update = () => {
     if(zIndex > obstacles.length - 1){
         ctx.drawImage(playerImage, spriteX*16, spriteY*32, 16, 32, x, y, 32, 64);
     }
-    
     tickCount++;
+
     if (tickCount > ticksPerFrame){
         tickCount = 0;
-        if (vy != 0 || vx != 0) {
+        if (isMoving) {
             spriteX=(spriteX+1)%4;
         } else {
             spriteX = 0;
@@ -87,7 +104,22 @@ update = () => {
     requestAnimationFrame(update)
 }
 
-
+moveCharacter = () => {
+    isMoving = false;
+    if (keysPressed.KeyW && lastKey == 'KeyW') {
+        y -= speed;
+        isMoving = true;
+    } else if (keysPressed.KeyA && lastKey == 'KeyA'){
+        x -= speed;
+        isMoving = true;
+    } else if (keysPressed.KeyS && lastKey == 'KeyS'){
+        y += speed;
+        isMoving = true;
+    } else if (keysPressed.KeyD && lastKey == 'KeyD'){
+        x += speed;
+        isMoving = true;
+    } 
+}
 sort = () => {
     for(let i = 0; i < obstacles.length; i++){
         if (obstacles[i][2] + 110 > y){
@@ -96,7 +128,6 @@ sort = () => {
     }
     return obstacles.length;
 }
-
 
 
 update();
