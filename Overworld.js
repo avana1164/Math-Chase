57078e68af2b2f39687e931a18373bce307efaf4
AAnand05 <44912634+avana1164@users.noreply.github.com>
2024-10-02 20:48:48 -0400

    Commit Restart #6
    Code for drawing the player object and tracking its movement has been moved to Person.js. A function animate has been created to control what frame should been drawn for the character.

--- Overworld.js
+++ Overworld.js
@@ -7,81 +7,57 @@ ctx.imageSmoothingEnabled = false;
 /* 1 2 3
    4 5 6
    7 8 9*/
-let directionInput = new DirectionInput();
+
 let obstacleKey = {'house': "/game_sprites/House.png"};
 let obstacles = [['house', 200, 25], ['house', 400, 25]];
-const playerImage = new Image();
+let player = new Person({x: 20, y: 30, speed: 2, src: "/game_sprites/walk.png", xDim: 16, yDim: 32});
 const field = new Image();
-playerImage.src = "/game_sprites/walk.png";
 field.src = "/game_sprites/grassy_field1.png";
-
-let x = 20;
-let y = 30;
-let speed = 2;
-
-let isMoving = false;
-
-let spriteX = 0;
 
 let tickCount = 0;
 const ticksPerFrame = 10;
 
 update = () => {
     ctx.clearRect(0, 0, c.width, c.height);
-    let lastKey = directionInput.lastKey;
-    moveCharacter(lastKey);
-    
-    let spriteY = directionInput.spriteDirection[lastKey][0];
     ctx.drawImage(field, 0, 0, 512, 256, 0, 0, 1024, 512);
+
+    player.moveCharacter();
+    
     zIndex = sort();
     for(let i = 0; i < obstacles.length; i++) {
         let obstacle = new Image();
         obstacle.src = obstacleKey[obstacles[i][0]];
         if (i == zIndex){
-            ctx.drawImage(playerImage, spriteX*16, spriteY*32, 16, 32, x, y, 32, 64);
+            player.draw(ctx);
         }
         ctx.drawImage(obstacle, 0, 0, 80, 80, obstacles[i][1], obstacles[i][2], 160, 160);
     }
     if(zIndex > obstacles.length - 1){
-        ctx.drawImage(playerImage, spriteX*16, spriteY*32, 16, 32, x, y, 32, 64);
+        player.draw(ctx);
     }
     tickCount++;
-
-    if (tickCount > ticksPerFrame){
-        tickCount = 0;
-        if (isMoving) {
-            spriteX=(spriteX+1)%4;
-        } else {
-            spriteX = 0;
-        }
-    }
+    animate();
     requestAnimationFrame(update)
 }
 
-moveCharacter = (lastKey) => {
-    isMoving = false;
-    if (directionInput.spriteDirection['KeyW'][1] && lastKey == 'KeyW') {
-        y -= speed;
-        isMoving = true;
-    } else if (directionInput.spriteDirection['KeyA'][1] && lastKey == 'KeyA'){
-        x -= speed;
-        isMoving = true;
-    } else if (directionInput.spriteDirection['KeyS'][1] && lastKey == 'KeyS'){
-        y += speed;
-        isMoving = true;
-    } else if (directionInput.spriteDirection['KeyD'][1] && lastKey == 'KeyD'){
-        x += speed;
-        isMoving = true;
-    } 
+animate = () => {
+    if (tickCount > ticksPerFrame){
+        tickCount = 0;
+        if (player.isMoving) {
+            player.spriteX=(player.spriteX+1)%4;
+        } else {
+            player.spriteX = 0;
+        }
+    }
 }
+
 sort = () => {
     for(let i = 0; i < obstacles.length; i++){
-        if (obstacles[i][2] + 110 > y){
+        if (obstacles[i][2] + 110 > player.y){
             return i;
         }     
     }
     return obstacles.length;
 }
-
 
 update();
--- Person.js
+++ Person.js
@@ -1,8 +1,35 @@
 class Person extends GameObject {
     constructor(config){
         super(config);
-        this.xDim = 16;
-        this.yDim = 32;
-        this.isPlayer = config.isPlayer || true;
+        this.directionInput = new DirectionInput();
+        this.isMoving = false;
+        this.spriteX = 0;
+        this.speed = config.speed;
+        //this.isPlayer = config.isPlayer || true;
+    }
+
+    moveCharacter = () => {
+        this.lastKey = this.directionInput.lastKey;
+        this.spriteY = this.directionInput.spriteDirection[this.lastKey][0];
+        this.isMoving = false;
+        if (this.directionInput.spriteDirection['KeyW'][1] && this.lastKey == 'KeyW') {
+            this.y -= this.speed;
+            this.isMoving = true;
+        } else if (this.directionInput.spriteDirection['KeyA'][1] && this.lastKey == 'KeyA'){
+            this.x -= this.speed;
+            this.isMoving = true;
+        } else if (this.directionInput.spriteDirection['KeyS'][1] && this.lastKey == 'KeyS'){
+            this.y += this.speed;
+            this.isMoving = true;
+        } else if (this.directionInput.spriteDirection['KeyD'][1] && this.lastKey == 'KeyD'){
+            this.x += this.speed;
+            this.isMoving = true;
+        } 
+    }
+
+    
+
+    draw = (ctx) => {
+        ctx.drawImage(this.sheet, this.spriteX*this.xDim, this.spriteY*this.yDim, this.xDim, this.yDim, this.x, this.y, this.xDim*2, this.yDim*2);
     }
 }
